0b7b4d1aa045ab7c5b6d407b0e94db7ef9933c31
KudaMukucha <kudamukucha80@gmail.com>
2024-03-29 12:03:57 +0200

    Added class for active category btn

--- src/components/HeroSearchSection.jsx
+++ src/components/HeroSearchSection.jsx
@@ -1,6 +1,8 @@
+import { useEffect } from 'react'
 import {HiMagnifyingGlass }from 'react-icons/hi2'
-import { Link } from 'react-router-dom'
+import { Link, useLocation } from 'react-router-dom'
 export default function HeroSearchSection() {
+    const location = useLocation()
     const searchButtonTags = [
         {
             id:1,
@@ -24,6 +26,10 @@ export default function HeroSearchSection() {
         },
 
     ]
+
+    useEffect(() => {
+      console.log(location);
+    }, [])
   return (
     <div className='flex flex-col items-center text-center space-y-4 bg-slate-100  '>
         <h2 className='mt-5 font-extrabold text-5xl'>USSD Finder for Zimbabwe</h2>
@@ -38,7 +44,7 @@ export default function HeroSearchSection() {
            {
             searchButtonTags.map((tag,i)=>{
                 return(
-                    <Link to={tag.path} key={i} className={'border border-slate-300 ml-2 px-4 py-2 rounded-md bg-white font- hover:text-blue-700 hover:border-blue-700 hover:font-semibold hover:scale-110 transition-all duration-300'}>
+                    <Link to={tag.path} key={i} className={`border border-slate-300 ml-2 px-4 py-2 rounded-md bg-white font- hover:text-blue-700 hover:border-blue-700 hover:font-semibold hover:scale-110 transition-all duration-300 ${location.pathname === tag.path ? 'border-blue-700':''}`}>
                             {tag.name}
                     </Link>
                 )
