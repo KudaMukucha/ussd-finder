3e6e97055add1bbd581b8f663d658e48de3c00df
KudaMukucha <kudamukucha80@gmail.com>
2024-03-29 09:54:18 +0200

    Footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,6 +1,13 @@
+import { Link } from "react-router-dom"
 
 export default function Footer() {
+  const currentYear = new Date().getFullYear()
   return (
-    <div>Footer</div>
+    <div className="h-20 bg-slate-800 items-center text-center">
+        <div className="pt-3 text-slate-50">
+        <p>&copy; {currentYear}</p>
+        <p>Built with &#x1F496; by <Link to={'https://www.linkedin.com/in/kudakwashe-mukucha-998b3b178/'} target={'_blank'}>kuda mukucha</Link></p>
+        </div>
+    </div>
   )
 }
