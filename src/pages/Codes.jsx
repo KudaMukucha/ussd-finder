870dd816c587193eb51eb69bcbdff55edc925c74
KudaMukucha <kudamukucha80@gmail.com>
2024-03-29 14:04:54 +0200

    loading functionality on codes page

--- src/pages/Codes.jsx
+++ src/pages/Codes.jsx
@@ -5,13 +5,15 @@ import CodeCard from '../components/CodeCard'
 import HeroSearchSection from "../components/HeroSearchSection";
 export default function Codes() {
   const [codes,setCodes] = useState([])
+  const [loading, setLoading] = useState(true);
   let params =useParams()
   const getCodes = async(category)=>{
     const response = await fetch(`http://localhost:3000/codes/`)
     const codesData = await response.json()
     const object = codesData.filter(obj => obj.category === `${category}`);
     setCodes(object)
-    console.log(object)
+    setLoading(false);
+    // console.log(object)
   }
 
   useEffect(() => {
@@ -22,12 +24,13 @@ export default function Codes() {
     <HeroSearchSection/>
     <div className="bg-gray-100 pt-4 pb-4">
       <div className="px-4 lg:px-14 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
-      {codes.map((code,i)=>{
-          return(
-            <CodeCard key={i} code ={code} />
-            
-          )
-        })}
+      {loading ? (
+            <div className="text-center font-bold text-gray-500 bg-gray-100 h-48">Loading,please wait...</div>
+          ) : (
+            codes.map((code, i) => (
+              <CodeCard key={i} code={code} />
+            ))
+          )}
       </div>
     </div>
     </>
