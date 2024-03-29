f653e6174a735469d374063f2856f4ff4d175ba3
KudaMukucha <kudamukucha80@gmail.com>
2024-03-29 09:31:33 +0200

    Open pick an app trigger from code click

--- src/components/CodeCard.jsx
+++ src/components/CodeCard.jsx
@@ -12,7 +12,7 @@ export default function CodeCard({code}) {
     </Link>
     <p className="mb-3 font-normal text-gray-500 dark:text-gray-400">{code.description}</p>
    <div className='flex items-center justify-between'>
-   <a href="#" className="inline-flex font-bold items-center text-slate-950 text-2xl hover:text-blue-600 hover:scale-105 transition-all duration-75">
+   <a href={`tel:${code.code}`} className="inline-flex font-bold items-center text-slate-950 text-2xl hover:text-blue-600 hover:scale-105 transition-all duration-75">
        {code.code}
         <svg className="w-3 h-3 ms-2.5 rtl:rotate-[270deg]" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 18 18">
             <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M15 11v4.833A1.166 1.166 0 0 1 13.833 17H2.167A1.167 1.167 0 0 1 1 15.833V4.167A1.166 1.166 0 0 1 2.167 3h4.618m4.447-2H17v5.768M9.111 8.889l7.778-7.778"/>
